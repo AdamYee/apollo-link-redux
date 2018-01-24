edb9c93906c3068d70aacc699c48255cf2f9fda9
Tim Fairbrother <tdfairbrother@gmail.com>
2018-01-24 10:18:07 +1000

    added subscription actions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { ApolloLink } from 'apollo-link';
-import { getMainDefinition } from 'apollo-utilities';
+import {ApolloLink} from 'apollo-link';
+import {getMainDefinition} from 'apollo-utilities';
 
 export const APOLLO_QUERY_INIT = 'APOLLO_QUERY_INIT';
 export const APOLLO_QUERY_RESULT = 'APOLLO_QUERY_RESULT';
 export const APOLLO_MUTATION_INIT = 'APOLLO_MUTATION_INIT';
 export const APOLLO_MUTATION_RESULT = 'APOLLO_MUTATION_RESULT';
+export const APOLLO_SUBSCRIPTION_INIT = 'APOLLO_SUBSCRIPTION_INIT';
+export const APOLLO_SUBSCRIPTION_RESULT = 'APOLLO_SUBSCRIPTION_RESULT';
 
 const payload = ({
   operationName,
@@ -40,8 +42,20 @@ export const mutationResult = (result, operation) => ({
   ...payload(operation)
 });
 
+export const subscriptionInit = operation => ({
+  type: APOLLO_SUBSCRIPTION_INIT,
+  ...payload(operation)
+});
+
+export const subscriptionResult = (result, operation) => ({
+  type: APOLLO_SUBSCRIPTION_RESULT,
+  result,
+  ...payload(operation)
+});
+
 const isQuery = op => op === 'query';
 const isMutation = op => op === 'mutation';
+const isSubscription = op => op === 'subscription';
 
 class ReduxLink extends ApolloLink {
   /**
@@ -60,12 +74,18 @@ class ReduxLink extends ApolloLink {
     else if (isMutation(definition.operation)) {
       this.store.dispatch(mutationInit(operation));
     }
+    else if (isSubscription(definition.operation)) {
+      this.store.dispatch(subscriptionInit(operation));
+    }
     return observer.map(result => {
       if (isQuery(definition.operation)) {
         this.store.dispatch(queryResult(result, operation));
       }
       else if (isMutation(definition.operation)) {
         this.store.dispatch(mutationResult(result, operation));
+      }
+      else if (isSubscription(definition.operation)) {
+        this.store.dispatch(subscriptionResult(result, operation));
       }
       return result;
     });
